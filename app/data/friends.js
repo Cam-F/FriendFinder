1e9b28acd1909a48d5b16af498dc075c776727dc
cameron fuller <cameronmfuller@yahoo.com>
2018-01-18 23:04:04 -0500

    dummy friends data

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        name: "Matthew McConaughey",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjJ59a8kePYAhVRaq0KHTCDCVwQjRwIBw&url=https%3A%2F%2Fwww.biography.com%2Fpeople%2Fmatthew-mcconaughey-16599996&psig=AOvVaw1BMegxiq3QVMsmOG2MqryG&ust=1516420675428322",
+        score: [1,2,3,4,5,5,4,3,2,1]
+    },
+    {
+        name: "Keanu Reeves",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiX_OSRkuPYAhUQd6wKHZilCWEQjRwIBw&url=http%3A%2F%2Fwww.forevernews.in%2Fkeanu-reeves-to-star-in-siberia-67123&psig=AOvVaw1nUpEYbyr_hdhihqDJAKLa&ust=1516420867861123",
+        score: [5,4,3,5,3,4,2,3,4,5]
+    },
+    {
+        name: "Mark Wahlberg",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiJlrimkuPYAhURG6wKHRm6D18QjRwIBw&url=https%3A%2F%2Fwww.popsugar.com%2FMark-Wahlberg&psig=AOvVaw1_o13HNtT6KcwRTlAJ5zSR&ust=1516420919448569",
+        score: [4,4,3,2,1,1,4,5,3,2]
+    }
+];
+
+module.exports = friends;
